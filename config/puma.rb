23e989ba2dde1ab46b91747fe356f162d3e252dc
feitian124 <feitian124@gmail.com>
2013-11-23 21:29:53 +0800

    change puma to use 2 wokers

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,7 @@ APP_ROOT = '/data/www/ruby-china/current'
 pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
 state_path "#{APP_ROOT}/tmp/pids/puma.state"
 daemonize true
-workers 1
+workers 2
 threads 2,8
 preload_app!
 
