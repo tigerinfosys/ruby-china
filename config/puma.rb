b841440d9504cc2111bd618cf347243f1e9db156
feitian124 <feitian124@gmail.com>
2013-11-26 21:49:30 +0800

    online resource title pull left

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,5 @@
 APP_ROOT = '/data/www/ruby-china/current'
+bind  "unix:///var/run/ruby-china.sock"
 pidfile "#{APP_ROOT}/tmp/pids/puma.pid"
 state_path "#{APP_ROOT}/tmp/pids/puma.state"
 daemonize true
